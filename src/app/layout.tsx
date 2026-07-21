084a55bff43df9a513cc24172e480c8349a6700e
Hellnight2005 <abhijeet2005shinde@gmail.com>
2026-07-21 14:14:33 +0530

    feat: add Microsoft Clarity tracking script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -64,6 +64,15 @@ export default function RootLayout({
             gtag('config', 'G-LWESM1V5Q0');
           `}
         </Script>
+        <Script id="clarity-script" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "xpu4f6o2f2");
+          `}
+        </Script>
       </body>
     </html>
   );
